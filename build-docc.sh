dd85ae6cf00c0e13ad75d43954cd1c773754f66f
Karen <957458041@qq.com>
2023-12-28 20:31:38 +0800

    update

--- build-docc.sh
+++ build-docc.sh
@@ -8,6 +8,6 @@ xcrun xcodebuild docbuild \
 xcrun docc process-archive transform-for-static-hosting \
     "$PWD/.derivedData/Build/Products/Debug-iphonesimulator/GivenWithLove.doccarchive" \
     --output-path ".docs" \
-    --hosting-base-path "" # add your repo name 
+    --hosting-base-path "DocC-Tutorial-for-Swift-Automating-Publishing-With-GitHub-Actions" 
     
 echo '<script>window.location.href += "/documentation/givenwithlove"</script>' > .docs/index.html
