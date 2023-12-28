9aebc1b47803a58c9abc710d121d915c18463348
Karen <957458041@qq.com>
2023-12-28 20:10:16 +0800

    update

--- build-docc.sh
+++ build-docc.sh
@@ -4,3 +4,8 @@ xcrun xcodebuild docbuild \
     -scheme GivenWithLove \
     -destination 'generic/platform=iOS Simulator' \
     -derivedDataPath "$PWD/.derivedData"
+
+xcrun docc process-archive transform-for-static-hosting \
+    "$PWD/.derivedData/Build/Products/Debug-iphonesimulator/GivenWithLove.doccarchive" \
+    --output-path ".docs" \
+    --hosting-base-path "" # add your repo name later
